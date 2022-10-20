9c7acd045836ea482bd1cf79e070478320f08ff6
Gabriel <gabrielloures21@gmail.com>
2022-10-20 11:43:31 -0300

    End ExpirationDate

--- src/main.js
+++ src/main.js
@@ -24,3 +24,34 @@ function setCardType(type) {
 
 globalThis.setCardType = setCardType; // colocamos a função no globalThis do navegador, para podermos utilizar no console do dev.tools => para executar no browser bastaria digitarmos no 'console' globalThis.setCardType("default") ou globalThis.setCardType("visa") ou globalThis.setCardType("mastercard")
 
+// Security Code Mask - Essa estrutura abaixo é uma estrutura padrão do IMask (olhar documentação depois) para definirmos como um input poderá ser digitado pelo usuário (nesse caso, o CVC de um cartão de crédito possui no máximo 4 dígitos, então colocamos "0000" => com isso, o usuário só poderá digitar números e no máximo 4 deles)
+
+const securityCode = document.querySelector('#security-code');
+
+const securityCodePattern = {
+  mask: "0000"
+}
+
+const securityCodeMask = IMask(securityCode, securityCodePattern)
+
+// Expiration Date
+
+const expirationDate = document.querySelector('#expiration-date')
+
+const expirationDatePattern = {
+  mask: 'MM{/}YY', // o que colocamos entre {} é colocado automaticamente quando o input vai sendo digitado pelo usuário
+  blocks: { // usamos o blocks (que é um conjunto de objetos) para definir parâmetros para nossas masks
+    MM: {
+      mask: IMask.MaskedRange,
+      from: 1,
+      to: 12
+    },
+    YY: {
+      mask: IMask.MaskedRange,
+      from: String(new Date().getFullYear()).slice(2), // função JavaScript que pega o ano atual; slice(x) pega os x últimos caracteres de uma string e retorna elas 
+      to: String(new Date().getFullYear() + 10).slice(2)
+    }
+  }
+}
+
+const expirationDateMask = IMask(expirationDate, expirationDatePattern)
